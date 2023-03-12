4671eb72ba34061ec63f85b4089b184e61a9c339
JKVaco <brute1904@gmail.com>
2023-03-12 17:56:13 +0500

    Added breadcrumb to reorder page

--- controllers/menucontroller/reorder.php
+++ controllers/menucontroller/reorder.php
@@ -1,1 +1,14 @@
+<?php
+Block::put('breadcrumb') ?>
+<ul>
+    <li>
+        <a href="<?= Backend::url('UrsacoreLab/Menu/MenuController') ?>">
+            <?= e(trans('ursacorelab.menu::lang.controller.label_plural')); ?>
+        </a>
+    </li>
+    <li><?= e($this->pageTitle) ?></li>
+</ul>
+<?php
+Block::endPut() ?>
+
 <?= $this->reorderRender() ?>
